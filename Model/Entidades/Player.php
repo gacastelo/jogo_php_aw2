43e5f9afe9a78d9f01dceda8a0083a432892d969
gacastelo <gabriel.castelo5949@gmail.com>
2026-08-26 23:24:42 -0300

    fix: now Player::decreaseBuffDuration affects the original array via the pointer &$buff, and uses the spread operator to buff args

--- Model/Entidades/Player.php
+++ Model/Entidades/Player.php
@@ -1,4 +1,6 @@
 <?php
+require "../AbsModel/AbsEntity.php";
+require "../Model/Itens/Consumivel.php";
 class Player extends AbsEntity
 {
     private array $equipamento = ["cabeca" => null, "peitoral" => null, "botas" => null, "mao_principal" => null, "mao_secundaria" => null];
@@ -6,7 +8,7 @@ class Player extends AbsEntity
 
     private array $buffs = ["dano" => ["value" => 0, "duration" => 0], "velocidade" => ["value" => 0, "duration" => 0], "chance_esquiva" => ["value" => 0, "duration" => 0]];
 
-    public function __construct(string $nome = "Héroi", string $link_imagem = "default_hero.jpg")
+    public function __construct(string $nome = "Héroi", string $link_imagem = "./img/default_hero.gif")
     {
         $this->nome = $nome;
         $this->vida_maxima = 100;
@@ -15,6 +17,7 @@ class Player extends AbsEntity
         $this->dano = 5;
         $this->chance_esquiva = 0;
         $this->link_imagem = $link_imagem;
+        $this->guardarItem(new Consumivel("Poçao de Cura Mínima", "Cura 10 de vida", ["metodo" => "heal", "args" => [10]]));
     }
 
     public function guardarItem($item): void
@@ -22,13 +25,14 @@ class Player extends AbsEntity
         $this->inventario[$item->getId()] = $item;
     }
 
-    public function useItem($item): void
+    public function useItem($itemId): void
     {
+        $item = $this->inventario[$itemId];
         if ($item instanceof \Equipamento) {
             $this->equip($item);
         }
         if ($item instanceof \Consumivel) {
-            $this->consume($item);
+            $this->consume($item->getId());
         }
     }
 
@@ -65,10 +69,10 @@ class Player extends AbsEntity
             $this->vida_atual += $amount;
         }
     }
-    public function buff(string $type,int $value, int $duration): void
-    {
-        $this->buffs[$type] = ["value" => $value, "duration" => $duration];
-    }
+        public function buff(string $type,int $value, int $duration): void
+        {
+            $this->buffs[$type] = ["value" => $value, "duration" => $duration];
+        }
 
     public function getBuffs(): array
     {
@@ -77,18 +81,33 @@ class Player extends AbsEntity
 
     public function decreaseBuffDuration(): void
     {
-        foreach ($this->buffs as $type) {
-            if ($type["duration"] > 0)
-            {
-                $this->buffs[$type]["duration"]--;
+        foreach ($this->buffs as $type => &$buff) {
+            if ($buff["duration"] > 0) {
+                $buff["duration"]--;
+            }
+
+            if ($buff["duration"] === 0) {
+                $buff["value"] = 0;
             }
         }
+
+        unset($buff);
+    }
+
+    public function decreaseAttacksCooldown(): void
+    {
+    //TODO: if make differents attacks make they cooldown here
     }
 
     public function consume(string $id): void
     {
         $consumivel = $this->inventario[$id];
         $efeito = $consumivel->getEfeito();
-        $this->$efeito;
+        $metodo = $efeito['metodo'];
+        $args = $efeito['args'];
+        $this->$metodo(...$args);
+        unset($this->inventario[$id]);
+        header("Location: combate.php");
+        exit();
     }
 }
